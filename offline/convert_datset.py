b644fddd746e32982a6cac86cb8d489eca8716d8
MHamza-Y <m.hamza.yousaf.22@gmail.com>
2022-08-09 16:49:17 +0200

    fixed memory bugs and optimized memory on data creation and loading.

--- offline/convert_datset.py
+++ offline/convert_datset.py
@@ -1,8 +1,14 @@
 import glob
 import json
+import os
 
+import h5py
 import numpy as np
-import pandas as pd
+
+
+def convert_dict_list_to_numpy(data):
+    for key in data:
+        data[key] = np.array(data[key])
 
 
 def load_rllib_dataset(glob_regex):
@@ -12,13 +18,15 @@ def load_rllib_dataset(glob_regex):
         with open(file) as f:
             for line in f:
                 episode_obj = json.loads(line)
+                convert_dict_list_to_numpy(episode_obj)
                 data.append(episode_obj)
 
     return data
 
 
 def save_json(out_path, data):
-    with open(out_path, 'w') as f:
+    with open(out_path, 'a', encoding="utf8") as f:
+        # save htfi
         json.dump(data, f)
 
 
@@ -41,55 +49,56 @@ def covert_episode_input(input_array, input_steps, output_steps):
     for count, value in enumerate(input_array):
         if count < len(input_array) - input_steps - output_steps:
             input_sample = input_array[count:count + input_steps]
-            input_sample.reverse()
             converted_input.append(input_sample)
 
-    return converted_input
+    return np.asarray(converted_input)
 
 
 def convert_episode_output(output_array, input_steps, output_steps):
     converted_output = []
     for count, value in enumerate(output_array):
         if input_steps <= count < len(output_array) - output_steps:
-            converted_output.append(output_array[count:count + output_steps])
+            output_sample = output_array[count:count + output_steps]
+            converted_output.append(output_sample)
 
-    return converted_output
-
-
-def merge_row(row):
-    print(row.shape)
-    return np.hstack(row)
+    return np.asarray(converted_output)
 
 
-def merge_multiple_features(data, features: list, out_key_name):
+def merge_multiple_features(data, features: list, out_key_name, out_dict):
     if len(features) > 1:
-        df = pd.DataFrame(data)
 
-        converted_rows = df[features].to_numpy(dtype=object)
-        # print(np.hstack(converted_rows[1]))
-        # converted_rows = numpy.transpose(converted_rows)
-        # print(converted_rows[0][1])
-        # print(converted_rows[1][0])
-        vf = np.vectorize(merge_row, signature="(n,m)->(k)")
-        # vf(converted_rows)
-        # merged_array = []
-        # for i in data_rows:
-        #
-        #     row = [data[feature][i] for feature in features]
-        #     row = np.hstack(row)
-        #     if i is 0:
-        #         merged_array = np.asarray([row])
-        #     else:
-        #         merged_array = np.append(merged_array, [row], axis=0)
-        # data[out_key_name] = merged_array.tolist()
+        merged_data = []
+        for feature in features:
+            merged_data.append(data[feature])
 
-        # data[out_key_name] = np.hstack(converted_rows).tolist()
-        # print(vf(converted_rows))
-        # data[out_key_name] = vf(converted_rows).tolist()
-        data[out_key_name] = [np.hstack(converted_rows[i]).tolist() for i in range(converted_rows.shape[0])]
+        merged_data = np.dstack(merged_data)
+    else:
+        merged_data = data[features[0]]
+
+    if out_key_name in out_dict:
+        out_dict[out_key_name] = np.append(out_dict[out_key_name], merged_data, axis=0)
+    else:
+
+        out_dict[out_key_name] = merged_data
 
 
-def create_lstm_dataset(rllib_files, out_path, input_steps, output_steps, input_keys=None, output_keys=None):
+def save_hdf(out_path, data, h5_key, write_mode='w'):
+    # df = pd.DataFrame(data)
+    # df.to_hdf(out_path, h5_key)
+    hf = h5py.File(out_path, write_mode)
+    grp = hf.create_group(h5_key)
+    for key in data:
+        grp.create_dataset(key, data=data)
+    hf.close()
+
+
+def save_numpy(out_path, data):
+    np.save(out_path, data)
+
+
+def create_lstm_dataset(rllib_files, out_path, input_steps, output_steps, h5_key, input_keys=None, output_keys=None, ):
+    if os.path.exists(out_path):
+        os.remove(out_path)
     if output_keys is None:
         output_keys = ["obs", "actions"]
 
@@ -97,26 +106,27 @@ def create_lstm_dataset(rllib_files, out_path, input_steps, output_steps, input_
         input_keys = ["obs", "actions"]
 
     data = load_rllib_dataset(rllib_files)
-    lstm_dataset = {}
-    for obj in data:
-
+    print(len(data))
+    merged_data = {}
+    for ep_id, obj in enumerate(data):
+        print(f"Episode: {ep_id}")
+        lstm_episode_input_dataset = {}
+        lstm_episode_output_dataset = {}
         for key, value in obj.items():
             if key in input_keys:
                 converted_input = covert_episode_input(value, input_steps, output_steps)
-                if key in lstm_dataset:
-                    lstm_dataset[key].extend(converted_input)
-                else:
-                    lstm_dataset[key] = converted_input
+
+                lstm_episode_input_dataset[key] = converted_input
 
             if key in output_keys:
                 converted_output = convert_episode_output(value, input_steps, output_steps)
-                key = "out_" + key
-                if key in lstm_dataset:
-                    lstm_dataset[key].extend(converted_output)
-                else:
-                    lstm_dataset[key] = converted_output
 
-    merge_multiple_features(lstm_dataset, input_keys, "merged_input")
-    merge_multiple_features(lstm_dataset, output_keys, "merged_output")
+                lstm_episode_output_dataset[key] = converted_output
 
-    save_json(out_path=out_path, data=lstm_dataset)
+        merge_multiple_features(lstm_episode_input_dataset, input_keys, "merged_input", merged_data)
+        merge_multiple_features(lstm_episode_output_dataset, output_keys, "merged_output", merged_data)
+
+    for key in merged_data:
+        print(np.shape(merged_data[key]))
+    # save_json(out_path=out_path, data=merged_data)
+    save_numpy(out_path=out_path, data=merged_data)
